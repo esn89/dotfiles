a96e82ef80d7f13c6ffd9a5abb1ceff1c1bdc199
Evan <evan.sin.ng@gmail.com>
2014-05-03 01:59:57 +0000

    added ranger and changed up dunst notifications

---
+++ bspwm/.config/ranger/colorschemes/jungle.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2009-2013  Roman Zimbelmann <hut@lepus.uberspace.de>
+# This software is distributed under the terms of the GNU GPL version 3.
+
+from ranger.gui.color import *
+from ranger.colorschemes.default import Default
+
+class Scheme(Default):
+    progress_bar_color = green
+    def use(self, context):
+        fg, bg, attr = Default.use(self, context)
+
+        if context.directory and not context.marked and not context.link:
+            fg = green
+
+        if context.in_titlebar and context.hostname:
+            fg = red if context.bad else blue
+
+        return fg, bg, attr
