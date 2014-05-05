9fb56cceac799866ab412038fb07abbc1437ade2
Evan <evan.sin.ng@gmail.com>
2014-05-05 01:12:45 +0000

    now shows notification in dunst when clicked

--- bspwm/.barScripts/pacmanNotifier/onClickUpdates.py
+++ bspwm/.barScripts/pacmanNotifier/onClickUpdates.py
@@ -1,12 +1,37 @@
-#!/bin/bash
+import os.path
+from subprocess import call
 
-export DISPLAY=:0
+if os.path.isfile('/tmp/numOfUpdates') == False:
+    call(["notify-send", "-u", "low", "You have no new updates"])
+else:
+    # read the updates line by line into a list
+    with open('/tmp/numOfUpdates') as f:
+        listOfPkgs = f.read().splitlines()
+    #print(listOfPkgs)
+    pkgNo = len(listOfPkgs)
 
-#am i connected?  This pings the default gateway
-online=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+    # now make a well-formed parameter to dunst:
+    #
+    title = "<span color='#D28B71'><u>Updates Available:</u></span>"
+    greyspan = "<span color='#B0B0B0'>"
+    greenspan = "<span color='#99CC99'>"
+    endspan = "</span>"
+    attribute = False
+    query = ""
+    for pkg in listOfPkgs:
+        if attribute == False:
+            query += str(greyspan)
+            attribute = not attribute
+        else:
+            query += str(greenspan)
+            attribute = not attribute
+        query += str(pkg)
+        if pkgNo > 1:
+            query += str('\\n')
+        query += str(endspan)
+        pkgNo = pkgNo - 1
+    #print(query)
+    call(["notify-send", "-u", "low", title, query])
 
-if [ "$online" == "ok" ];
-then
 
-	eval pacman -Qu > /tmp/numOfUpdates
-fi
+
